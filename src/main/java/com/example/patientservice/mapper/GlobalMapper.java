8d6d5a57903cb4f6601a8f24f50266082d17d217
Aditya Wadkar <aditya.wadkar@volvo.com>
2025-05-14 11:24:41 +0530

    Modified GlobalMapper , implemented mapToResponseDto mapping data retrived from repo to PatientReponse reture to the client.

--- src/main/java/com/example/patientservice/mapper/GlobalMapper.java
+++ src/main/java/com/example/patientservice/mapper/GlobalMapper.java
@@ -1,6 +1,7 @@
 package com.example.patientservice.mapper;
 
 import com.example.patientservice.dto.PatientRequest;
+import com.example.patientservice.dto.PatientResponse;
 import com.example.patientservice.model.Patient;
 
 public class GlobalMapper {
@@ -11,6 +12,17 @@ public class GlobalMapper {
                 .phone_number(patientRequest.getPhone_number())
                 .gender(patientRequest.getGender())
                 .dateOfBirth(patientRequest.getDateOfBirth())
+                .build();
+    }
+
+    public PatientResponse mapToResposeDto(Patient patient) {
+        return PatientResponse.builder()
+                .patientId(patient.getPatientId())
+                .patientName(patient.getPatientName())
+                .patient_email(patient.getPatient_email())
+                .phone_number(patient.getPhone_number())
+                .gender(patient.getGender())
+                .dateOfBirth(patient.getDateOfBirth())
                 .build();
     }
 }
