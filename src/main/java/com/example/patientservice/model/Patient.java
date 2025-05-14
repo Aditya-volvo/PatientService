6a4c7fde843077586d0479f345d90abe5c2e7a04
Aditya Wadkar <aditya.wadkar@volvo.com>
2025-05-14 09:50:45 +0530

    created Patient entity which will be mapped in to the Database.

---
+++ src/main/java/com/example/patientservice/model/Patient.java
@@ -0,0 +1,40 @@
+package com.example.patientservice.model;
+
+import com.example.patientservice.enums.Gender;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "patient_table")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "patient_id")
+    private int patientId;
+
+    @Column(name = "patient_name")
+    private String patientName;
+
+    @Column(name = "p_pho_no")
+    private String phone_number;
+
+    @Column(name = "patient_email")
+    private String patient_email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "patient_gender")
+    private Gender gender;
+
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+
+}
