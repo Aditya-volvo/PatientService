5dce3090ae5b76c5282793bd50211c9cd03dca8e
Aditya Wadkar <aditya.wadkar@volvo.com>
2025-05-14 11:21:14 +0530

    Modified PatientRepositiory , implemented findAllPatients database operation.

--- src/main/java/com/example/patientservice/repository/PatientRepositiory.java
+++ src/main/java/com/example/patientservice/repository/PatientRepositiory.java
@@ -3,5 +3,8 @@ package com.example.patientservice.repository;
 import com.example.patientservice.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PatientRepositiory extends JpaRepository<Patient, Integer> {
+    List<Patient> findAllPatients();
 }
